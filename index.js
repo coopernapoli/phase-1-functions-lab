5b9e6c127d4e1f784c9694d9979828b24244fb24
Sean <napolisc10@gmail.com>
2022-12-14 11:12:40 -0700

    commit tree 8a9c130f6f8444fb1ba0affe9be1f08a2a8cbf1e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const hq=42
+
+function distanceFromHqInBlocks(pickup)//initialize finction
+{
+
+return Math.abs(pickup - hq); //return the absolute value of the distance in blocks
+}
+
+
+
+const feet = 264
+
+function distanceFromHqInFeet(pickup){
+
+return Math.abs((pickup - hq)*feet);
+}
+
+function distanceTravelledInFeet(destination, pickup){
+    return Math.abs((pickup - destination)*feet)
+}
+
+function calculatesFarePrice (pickup, destination){
+
+    const distance = Math.abs((pickup - destination)*feet)
+    if (distance < 400) {
+        return 0;
+    };
+    if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    };
+    if (distance > 2000 && distance < 2500) {
+        return 25;
+    };
+    if (distance > 2500) {
+        return "cannot travel that far";
+    };
+}
